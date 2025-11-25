c805afe72a0e1241b05b91b68e4418d6dd8ecbf5
Hornet <husteyteepek@gmail.com>
2025-11-25 15:59:29 +0100

    skoro hotovy retizek, test

---
+++ Test/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Xml.Serialization;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int bod1x;
+                string point1x = Console.ReadLine();
+                if (point1x == "q")
+                {
+                    break;
+                }
+                else
+                {
+                    bod1x = Convert.ToInt32(point1x);
+                }
+                int bod1y = Convert.ToInt32(Console.ReadLine());
+                int bod2x = Convert.ToInt32(Console.ReadLine());
+                int bod2y = Convert.ToInt32(Console.ReadLine());
+                int bod3x = Convert.ToInt32(Console.ReadLine());
+                int bod3y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                double stranaA = Vzdalenost(bod1x, bod1y, bod2x, bod2y);
+                double stranaB = Vzdalenost(bod3x, bod3y, bod2x, bod2y);
+                double stranaC = Vzdalenost(bod1x, bod1y, bod3x, bod3y);
+
+                trojNerovnost(stranaA, stranaB, stranaC);
+            }
+        }
+        public static double Vzdalenost(int bod1x, int bod1y, int bod2x, int bod2y)
+        {
+            int vzdalenostX = bod1x - bod2x;
+            int vzdalenostY = bod1y - bod2y;
+            double vzdalenost = Math.Sqrt(Math.Pow(vzdalenostX, 2) + Math.Pow(vzdalenostY, 2));
+            return vzdalenost;
+        }
+        public static void trojNerovnost(double A, double B, double C) 
+        {
+            if (A + B <= C || A + C <= B || B + C <= A)
+            {
+                Console.WriteLine("Tyto tři body netvoří trojúhelník");
+            }
+            else
+            {
+                Console.WriteLine(A);
+                Console.WriteLine(B);
+                Console.WriteLine(C);
+            }
+        }
+    }
+}
---
+++ retizekPratelstvi/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace retizekPratelstvi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lidi = Convert.ToInt32(Console.ReadLine());
+            string spojnice = Console.ReadLine();
+            string cil = Console.ReadLine();
+            string[] dvojice = spojnice.Split();
+
+            string[] path = cil.Split();
+            int start = Convert.ToInt32(path[0]);
+            int target = Convert.ToInt32(path[1]);
+
+            int[,] matice = new int[lidi,lidi];
+
+            for (int i = 0; i < dvojice.Length; i++)
+            {
+                string[] rozdeleni = dvojice[i].Split("-");
+                int vrchol1 = Convert.ToInt32(rozdeleni[0]);
+                int vrchol2 = Convert.ToInt32(rozdeleni[1]);
+
+                matice[vrchol1-1, vrchol2-1] = 1;
+                matice[vrchol2-1, vrchol1-1] = 1;
+            }
+            for (int i = 0; i < lidi; i++) 
+            {
+                for (int j = 0; j < lidi; j++)
+                {
+                    Console.Write(matice[i,j]);
+                }
+                Console.WriteLine();
+            }
+            PathSearch(lidi, matice, start, target);
+        }
+        public static void PathSearch(int maticeSize, int[,] matice,int start, int target)
+        {
+            Queue<int> otevreneVrcholy = new Queue<int>();
+            // otevreneVrcholy.Enqueue(1);
+            // otevreneVrcholy.Dequeue();
+            // otevreneVrcholy.Peek();
+            int?[] stav = new int?[maticeSize];//0-nenalezeny, 1-otevreny, 2-uzavreny
+            for(int i = 0; i < maticeSize; i++)
+            {
+                stav[i] = 0;
+            }
+            //List<int> stav = new List<int>();
+            //for (int i = 0; i < maticeSize; i++)
+            //{
+            //    stav.Add(0);
+            //}
+            List<int> predchudce = new List<int>();
+            for (int i = 0; i < maticeSize; i++)
+            {
+                predchudce.Add(0);
+            }
+
+            otevreneVrcholy.Enqueue(start);
+
+            stav[start-1] = 1;
+            int distance = 0;
+            List<int> cesta = new List<int>();
+
+
+            while (otevreneVrcholy.Count > 0)
+            {
+                //if (otevreneVrcholy.Peek() == maticeSize + 1)
+                //{
+                //    distance++;
+                //    otevreneVrcholy.Dequeue();
+                //    if (otevreneVrcholy.Count != 0)
+                //    {
+                //        otevreneVrcholy.Enqueue(maticeSize + 1);
+                //    }
+                //}
+                //else
+                {
+                    int pointIndex = otevreneVrcholy.Peek() - 1;
+                    for (int i = 0; i < maticeSize; i++)
+                    {
+                        if (matice[pointIndex, i] == 1 && stav[i] == 0)
+                        {
+                            otevreneVrcholy.Enqueue(i + 1);
+                            stav[i] = 1;
+                            predchudce[i] = otevreneVrcholy.Peek();
+                        }
+                    }
+                    if (otevreneVrcholy.Peek() == target)
+                    {
+                        int predtim = target-1;
+                        while (predchudce[predtim] != start)
+                        {
+                            cesta.Add(predtim + 1);
+                            predtim = predchudce[predtim]-1;
+                            distance++;
+                        }
+                        for(int i = cesta.Count-1; i >= 0; i--)
+                        {
+                            Console.Write(cesta[i]);
+                        }
+                    }
+                    stav[pointIndex] = 2;
+                    otevreneVrcholy.Dequeue();
+                }
+            }
+            Console.WriteLine(distance-1);
+            
+        }
+    }
+}
